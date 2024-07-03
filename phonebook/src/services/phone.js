810abc5f555679b2f14a24e4da24999f14c8ffb0
iamprincetj <tochukwunwanze5@gmail.com>
2024-07-04 00:01:21 +0100

    updated

--- phonebook/src/services/phone.js
+++ phonebook/src/services/phone.js
@@ -1,26 +1,24 @@
-import axios from "axios"
-const baseUrl = 'http://localhost:3001/api/persons'
-
+import axios from 'axios'
+const baseUrl = '/api/persons'
 
 const getPersons = async () => {
-    const request = await axios.get(baseUrl)
-    return request.data
+  const request = await axios.get(baseUrl)
+  return request.data
 }
-
 
 const createPerson = async (data) => {
-    const request = await axios.post(baseUrl, data)
-    return request.data
+  const request = await axios.post(baseUrl, data)
+  return request.data
 }
 
-const deletePerson =  (id) => {
-    const request = axios.delete(`${baseUrl}/${id}`)
-    return request.then(response => response.data)
+const deletePerson = (id) => {
+  const request = axios.delete(`${baseUrl}/${id}`)
+  return request.then((response) => response.data)
 }
 
 const updatePerson = async (id, data) => {
-    const request = axios.put(`${baseUrl}/${id}`, data)
-    return request.then(response => response.data)
+  const request = axios.put(`${baseUrl}/${id}`, data)
+  return request.then((response) => response.data)
 }
 
 export default { getPersons, createPerson, deletePerson, updatePerson }
